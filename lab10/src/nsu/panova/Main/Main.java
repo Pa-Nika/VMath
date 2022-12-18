8ee96375f74d276ac238511943fd219a910f9c4a
Pa-Nika <verun2001@mail.ru>
2022-12-19 00:49:43 +0700

    start non line

--- lab10/src/nsu/panova/Main/Main.java
+++ lab10/src/nsu/panova/Main/Main.java
@@ -1,12 +1,17 @@
 package nsu.panova.Main;
 
+import nsu.panova.Main.NonLineral.Cross;
+
 public class Main {
 
     public static void main(String[] args) {
-	    Executor executor = new Executor();
-        executor.work();
+//	    Executor executor = new Executor();
+//        executor.work();
+//
+//        Implicit implicit = new Implicit();
+//        implicit.work();
 
-        Implicit implicit = new Implicit();
-        implicit.work();
+        Cross cross = new Cross();
+        cross.work();
     }
 }
